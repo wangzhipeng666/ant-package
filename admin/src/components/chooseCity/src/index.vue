644ad9e143007417e0408b58fcb78e86045426f4
汪智鹏 <1848993253@qq.com>
2023-09-20 17:05:05 +0800

    城市选择弹出框

--- admin/src/components/chooseCity/src/index.vue
+++ admin/src/components/chooseCity/src/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>123</div>
+  <a-popover trigger="click" v-model:open="visible" placement="bottomLeft">
+    <template #content>
+      <p>Content</p>
+      <p>Content</p>
+    </template>
+    <div class="popover-desc">
+      <span>{{ result }}</span>
+      <DownOutlined :class="{ 'rotate': visible}"/>
+    </div>
+  </a-popover>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+import { DownOutlined } from '@ant-design/icons-vue';
+
+const visible = ref(false);
+const result = ref('请选择');
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.popover-desc {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  cursor: pointer;
+  span {
+    margin-right: 5px;
+  }
+  .rotate {
+    transform: rotate(180deg);
+  }
+}
+</style>
